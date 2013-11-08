16c657084eb18c932410e3f05cbdd98e342bdaa2
huangfuyang <huangfuyang@gmail.com>
2013-11-08 15:19:13 +0800

    tommy branch

--- LauncherCMD/LauncherCMD.cs
+++ LauncherCMD/LauncherCMD.cs
@@ -12,6 +12,7 @@ namespace CURELab.SignLanguage.Launcher
     {
         static void Main(string[] args)
         {
+			//tommy update
             Console.WriteLine("this is a launcher without kinect");
             RecognitionController lc = new RecognitionController();
             lc.BeginTest();
